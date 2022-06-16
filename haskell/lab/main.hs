2e4b84953efb19943d28a0e6be98a57b4401b839
Ivan Dyakin <vvhappyguy@gmail.com>
2022-06-16 23:39:12 +0300

    Fixed bug with  subtraction operation

--- haskell/lab/main.hs
+++ haskell/lab/main.hs
@@ -90,7 +90,7 @@ solve = head . Prelude.foldl foldingFunction [] . words
     where 
         foldingFunction (x:y:ys) "*" = (x * y):ys
         foldingFunction (x:y:ys) "+" = (x + y):ys
-        foldingFunction (x:y:ys) "-" = (x - y):ys
+        foldingFunction (x:y:ys) "-" = (y - x):ys
         foldingFunction (x:y:ys) "/" = (y / x):ys
         foldingFunction xs numberString = read numberString:xs
 
